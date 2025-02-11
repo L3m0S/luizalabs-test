8b959237ed82391afae9cff3b9162355b61ed442
g.lemos <gabriel_lemos06@hotmail.com>
2025-02-11 20:13:26 -0300

    style: fix indentation in files

--- src/presentation/express/middlewares/ErrorHandler.ts
+++ src/presentation/express/middlewares/ErrorHandler.ts
@@ -1,24 +1,28 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
-import { ApiError } from "../../../main/helpers/ApiError";
-import { IErrorResponse } from "@presentation/http/interfaces/IErrorResponse";
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { IErrorResponse } from '@presentation/http/interfaces/IErrorResponse';
+import { ApiError } from '@application/helpers/ApiError';
 
-const errorHandler: ErrorRequestHandler = (error: Error & Partial<ApiError>, req: Request, res: Response, next: NextFunction) => {
+const errorHandler: ErrorRequestHandler = (
+	error: Error & Partial<ApiError>,
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const DEFAULT_STATUS_CODE = 500;
+	const DEFAULT_ERROR_MESSAGE = 'Internal Server Error';
 
-  const DEFAULT_STATUS_CODE = 500;
-  const DEFAULT_ERROR_MESSAGE = 'Internal Server Error';
+	const statusCode = error?.statusCode ?? DEFAULT_STATUS_CODE;
 
-  const statusCode = error?.statusCode ?? DEFAULT_STATUS_CODE;
+	const errorMessage: IErrorResponse = {
+		type: error.name,
+		status: statusCode,
+		title: error?.message ?? DEFAULT_ERROR_MESSAGE,
+		detail: error.message,
+		instance: req.url,
+	};
 
-  const errorMessage: IErrorResponse = {
-    type: error.name,
-    status: statusCode,
-    title: error?.message ?? DEFAULT_ERROR_MESSAGE,
-    detail: error.message,
-    instance: req.url
-  }
-
-  console.log(error);
-  res.status(statusCode).json({ message: errorMessage });
-}
+	console.log(error);
+	res.status(statusCode).json({ message: errorMessage });
+};
 
 export { errorHandler };
