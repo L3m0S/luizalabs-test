bb6b78f9b6af9c32257d67e8f7ff4fd89e0d30f3
g.lemos <gabriel_lemos06@hotmail.com>
2025-02-11 20:19:56 -0300

    chorus: add some libraries to the project and build script
    -Added axios for external http calls
    -Added build script
    -Added opossum circuit breaker
    -Added tsrynge for depdency injection
    -Added a file to register the classes on the depedency container

---
+++ src/config/container/index.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+import { container } from 'tsyringe';
+import { IProdutcsApiProvider } from '@application/interfaces/IProductsApiProvider';
+import { IProductService } from '@application/interfaces/IProductService';
+import { ProductService } from '@application/services/ProductService';
+import { ProductsApiProvider } from '@infra/providers/ProductsApiProvider';
+import { ProductController } from '@presentation/http/controllers/ProductController';
+import { ICustomerService } from '@application/interfaces/ICustomerService';
+import { CustomerService } from '@application/services/CustomerService';
+import { ICustomerFavoriteProductService } from '@application/interfaces/ICustomerFavoriteProductsService';
+import { CustomerFavoriteProductService } from '@application/services/CustomerFavoriteProductsService';
+import { ICircuitBreakerProvider } from '@application/interfaces/ICircuitBreakerProvider';
+import { OpossumCircuitBreakerProvider } from '@infra/providers/OpossumCircuitBreakerProvider';
+
+// Map interfaces to implementations using tokens
+
+container.register('ProductController', {
+  useClass: ProductController,
+});
+
+container.register<IProductService>('IProductService', {
+  useClass: ProductService,
+});
+
+container.register<IProdutcsApiProvider>('IProdutcsApiProvider', {
+  useClass: ProductsApiProvider,
+});
+
+container.register<ICustomerService>('ICustomerService', {
+  useClass: CustomerService,
+});
+
+container.register<ICustomerFavoriteProductService>('ICustomerFavoriteProductService', {
+  useClass: CustomerFavoriteProductService,
+});
+
+container.register<ICircuitBreakerProvider>('ICircuitBreakerProvider', {
+  useClass: OpossumCircuitBreakerProvider,
+});
