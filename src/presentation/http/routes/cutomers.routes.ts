415175ffa83eb8ba94ed8e3db55a7d6849e4b5a2
g.lemos <gabriel_lemos06@hotmail.com>
2025-02-11 20:25:26 -0300

    feat: add CustomerFavoriteProduct routes on Customer routes.

--- src/presentation/http/routes/cutomers.routes.ts
+++ src/presentation/http/routes/cutomers.routes.ts
@@ -2,8 +2,10 @@ import 'reflect-metadata';
 import { Router } from 'express';
 import { container } from 'tsyringe';
 import { CustomerController } from '@presentation/http/controllers/CustomerController';
+import { CustomerFavoriteProductController } from '../controllers/CustomerFavoriteProductController';
 
 const customerController: CustomerController = container.resolve(CustomerController);
+const customerFavoriteProductController = container.resolve(CustomerFavoriteProductController);
 
 export default (router: Router): void => {
   const customersRouter = Router();
@@ -18,4 +20,21 @@ export default (router: Router): void => {
   customersRouter.put('/:id', customerController.update.bind(customerController));
 
   customersRouter.get('/', customerController.getAllPaginated.bind(customerController));
+
+  customersRouter.post(
+    '/:customerId/favorites',
+    customerFavoriteProductController.create.bind(customerFavoriteProductController),
+  );
+
+  customersRouter.delete(
+    '/:customerId/favorites/:favoriteId',
+    customerFavoriteProductController.deleteById.bind(customerFavoriteProductController),
+  );
+
+  customersRouter.get(
+    '/:customerId/favorites',
+    customerFavoriteProductController.findAllByCustomerIdPaginated.bind(
+      customerFavoriteProductController,
+    ),
+  );
 };
