4c2daea9b7b112af03aebaf30ba4804bf9e05170
g.lemos <gabriel_lemos06@hotmail.com>
2025-02-12 18:23:59 -0300

    docs: add docs routes

---
+++ src/presentation/http/routes/swagger.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { serve, setup } from 'swagger-ui-express';
+const swaggerFile = require('../../../../swagger.json');
+
+export default (router: Router): void => {
+	const docsRouter = Router();
+	docsRouter.use('/', serve);
+	router.use('/api-docs', docsRouter);
+
+	docsRouter.get('/', setup(swaggerFile));
+};
